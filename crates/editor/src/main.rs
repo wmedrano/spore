afda2166bf2b25bdb813e584e9ec0f4cd29db2e1
Will Medrano <will.s.medrano@gmail.com>
2024-09-15 21:36:54 -0700

    Don't panic in main

--- crates/editor/src/main.rs
+++ crates/editor/src/main.rs
@@ -1,5 +1,3 @@
-use std::io;
-
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind},
     widgets::Paragraph,
@@ -7,7 +5,7 @@ use ratatui::{
 };
 use spore_vm::{Vm, VmSettings};
 
-fn main() -> io::Result<()> {
+fn main() -> anyhow::Result<()> {
     let mut terminal = ratatui::init();
     terminal.clear()?;
     let app_result = run(terminal);
@@ -15,21 +13,18 @@ fn main() -> io::Result<()> {
     app_result
 }
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+fn run(mut terminal: DefaultTerminal) -> anyhow::Result<()> {
     let mut vm = Vm::new(VmSettings {
         enable_aggressive_inline: false,
     });
-    vm.eval_str(
-        "(define (greeting) (list \"Hello Spore!\" \"Press 'q' to quit.\" (working-directory)))",
-    )
-    .unwrap();
+    let greeting_src = r#"(define (greeting)
+                              (list "Hello Spore!" "Press 'q' to quit." (working-directory)))"#;
+    vm.eval_str(greeting_src)?;
     loop {
-        let message = vm.eval_str("(greeting)").unwrap().to_string();
+        let message = vm.eval_str("(greeting)")?.to_string();
         terminal.draw(move |frame| {
-            let greeting = Paragraph::new(message.as_str());
-            frame.render_widget(greeting, frame.area());
+            frame.render_widget(Paragraph::new(message.as_str()), frame.area());
         })?;
-
         if let event::Event::Key(key) = event::read()? {
             if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
                 return Ok(());
