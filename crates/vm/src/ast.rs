781e013f14a413a6a467ba330cdd0a2247fb8ed1
Will Medrano <will.s.medrano@gmail.com>
2024-09-18 19:45:09 -0700

    Optimize parsing.
    - Check for leading digit before running int/float parsing functions.
    - Store strings in a CompactString.

--- crates/vm/src/ast.rs
+++ crates/vm/src/ast.rs
@@ -1,3 +1,4 @@
+use compact_str::CompactString;
 use thiserror::Error;
 
 use crate::tokenizer::{Token, TokenType};
@@ -29,7 +30,7 @@ pub enum Node<'a> {
     /// A node containing a float literal.
     Float(f64),
     /// A node containing a string literal.
-    String(String),
+    String(CompactString),
     /// A node containing a sub-tree.
     Tree(Vec<Node<'a>>),
 }
@@ -119,7 +120,7 @@ impl<'a> Node<'a> {
                 unreachable!()
             }
             TokenType::String => {
-                let mut res = String::with_capacity(contents.len().saturating_sub(2));
+                let mut res = CompactString::with_capacity(contents.len().saturating_sub(2));
                 let mut escaped = false;
                 for ch in contents[1..contents.len() - 1].chars() {
                     if escaped {
@@ -144,11 +145,18 @@ impl<'a> Node<'a> {
                 Node::String(res)
             }
             TokenType::Other => {
-                if let Ok(int) = contents.parse() {
-                    return Node::Int(int);
-                } else if let Ok(float) = contents.parse() {
-                    return Node::Float(float);
-                };
+                let has_leading_number = contents
+                    .chars()
+                    .next()
+                    .map(|ch| ch.is_digit(10))
+                    .unwrap_or(false);
+                if has_leading_number {
+                    if let Ok(int) = contents.parse() {
+                        return Node::Int(int);
+                    } else if let Ok(float) = contents.parse() {
+                        return Node::Float(float);
+                    }
+                }
                 match contents {
                     "void" => Node::Void,
                     "true" => Node::Bool(true),
@@ -181,7 +189,7 @@ mod tests {
                 Node::Int(1),
                 Node::Float(2.0),
                 Node::Identifier("three"),
-                Node::String("four".to_string()),
+                Node::String("four".into()),
                 Node::Bool(true),
                 Node::Bool(false),
             ]
@@ -203,13 +211,13 @@ mod tests {
                         Node::Identifier("a"),
                         Node::Identifier("b")
                     ]),
-                    Node::String("number".to_string()),
+                    Node::String("number".into()),
                 ]),
                 Node::Tree(vec![
                     Node::Identifier("str-len"),
-                    Node::String("str".to_string())
+                    Node::String("str".into())
                 ]),
-                Node::String("atom".to_string()),
+                Node::String("atom".into()),
             ]
         );
     }
@@ -218,24 +226,21 @@ mod tests {
     fn quoted_strings_within_strings_are_preserved() {
         let src = "\"this \\\"is\\\" a string\"";
         let actual = Node::parse_to_vec(src).unwrap();
-        assert_eq!(
-            actual,
-            vec![Node::String("this \"is\" a string".to_string())]
-        );
+        assert_eq!(actual, vec![Node::String("this \"is\" a string".into())]);
     }
 
     #[test]
     fn backslash_with_n_produces_newline() {
         let src = "\"\\nn\\n\"";
         let actual = Node::parse_to_vec(src).unwrap();
-        assert_eq!(actual, vec![Node::String("\nn\n".to_string())]);
+        assert_eq!(actual, vec![Node::String("\nn\n".into())]);
     }
 
     #[test]
     fn backslash_with_t_produces_tab() {
         let src = "\"\\tt\\t\"";
         let actual = Node::parse_to_vec(src).unwrap();
-        assert_eq!(actual, vec![Node::String("\tt\t".to_string())]);
+        assert_eq!(actual, vec![Node::String("\tt\t".into())]);
     }
 
     #[test]
