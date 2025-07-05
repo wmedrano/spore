338c89e2e7012d12d8a47e80463d57f2bd94cecd
wmedrano <will@wmedrano.dev>
2025-07-04 22:43:15 -0700

    Add docs and unit tests.

--- src/Val.zig
+++ src/Val.zig
@@ -5,22 +5,27 @@ const Val = @This();
 
 repr: ValRepr,
 
+// Create a new `Val` from its internal representation. For internal use only.
 fn init(repr: ValRepr) Val {
     return .{ .repr = repr };
 }
 
+// Create a new `Val` that holds an integer.
 pub fn new_int(int: i64) Val {
     return init(.{ .int = int });
 }
 
+// Create a new `Val` that holds a float.
 pub fn new_float(float: f64) Val {
     return init(.{ .float = float });
 }
 
+// Format a `Val` for printing.
 pub fn format(self: Val, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
     try self.repr.format(fmt, options, writer);
 }
 
+// The internal representation of a value.
 const ValRepr = union(enum) {
     int: i64,
     float: f64,
--- src/Vm.zig
+++ src/Vm.zig
@@ -8,26 +8,38 @@ const MAX_STACK_SIZE = 1024;
 stack_len: usize = 0,
 stack_data: [MAX_STACK_SIZE]Val = undefined,
 
+// Create a new VM.
 pub fn init() Vm {
     return .{};
 }
 
+// Get the complete stack for the `Vm`. The last value in the slice corresponds
+// to the top of the stack.
 pub fn stack(self: Vm) []const Val {
     return self.stack_data[0..self.stack_len];
 }
 
-pub fn pushVal(self: *Vm, val: Val) void {
+// Push `val` onto the stack.
+pub fn pushVal(self: *Vm, val: Val) !void {
+    if (self.stack_len == MAX_STACK_SIZE) {
+        return error.StackOverflow;
+    }
     self.stack_data[self.stack_len] = val;
     self.stack_len += 1;
 }
 
-pub fn pushVals(self: *Vm, vals: []const Val) void {
+// Push several `vals` onto the Vm. The last value of `vals` will be on top.
+pub fn pushVals(self: *Vm, vals: []const Val) !void {
     for (vals) |v| {
-        self.pushVal(v);
+        try self.pushVal(v);
     }
 }
 
-pub fn popVal(self: *Vm) Val {
+// Pop a `Val` from thes stack.
+pub fn popVal(self: *Vm) !Val {
+    if (self.stack_len == 0) {
+        return error.StackUnderflow;
+    }
     self.stack_len -= 1;
     return self.stack_data[self.stack_len];
 }
@@ -39,13 +51,26 @@ test "initial stack is empty" {
 
 test "push val adds to stack" {
     var vm = Vm.init();
-    vm.pushVals(&.{ Val.new_int(1), Val.new_int(2) });
+    try vm.pushVals(&.{ Val.new_int(1), Val.new_int(2) });
     try testing.expectFmt("{ 1, 2 }", "{any}", .{vm.stack()});
+}
+
+test "push to many vals returns stack overflow" {
+    var vm = Vm.init();
+    for (0..MAX_STACK_SIZE) |_| {
+        try vm.pushVal(Val.new_int(1));
+    }
+    try testing.expectError(error.StackOverflow, vm.pushVal(Val.new_int(1)));
 }
 
 test "pop val removes from stack" {
     var vm = Vm.init();
-    vm.pushVals(&.{ Val.new_int(1), Val.new_int(2), Val.new_int(3) });
+    try vm.pushVals(&.{ Val.new_int(1), Val.new_int(2), Val.new_int(3) });
     try testing.expectEqualDeep(Val.new_int(3), vm.popVal());
     try testing.expectFmt("{ 1, 2 }", "{any}", .{vm.stack()});
+}
+
+test "pop empty stack returns stack underflow" {
+    var vm = Vm.init();
+    try testing.expectError(error.StackUnderflow, vm.popVal());
 }
